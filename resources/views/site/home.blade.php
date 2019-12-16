06971bafeee097bbda714b3022946aae4bc8e58a
jrussellzau <50782167+jrussellzau@users.noreply.github.com>
2019-12-16 13:26:00 -0600

    Update home.blade.php

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -9,7 +9,7 @@ Welcome
     <div class="card o-hidden border-0 shadow-lg my-5">
         <div class="row no-gutters">
           <div class="col-lg-5">
-			<img src="/photos/frontpage.png" class="card-img" alt="...">
+			<img src="/photos/frontpage.png" class="card-img img-fluid" alt="...">
 			</div>
           <div class="col-lg-7">
 			<div class="card-body p-5">
