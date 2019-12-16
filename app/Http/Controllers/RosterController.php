6014add1ffaa210f62af5a54dadd458040934f2f
jrussellzau <50782167+jrussellzau@users.noreply.github.com>
2019-12-16 09:26:47 -0600

    Update RosterController.php

--- app/Http/Controllers/RosterController.php
+++ app/Http/Controllers/RosterController.php
@@ -35,13 +35,13 @@ class RosterController extends Controller
         if(Config::get('app.debug') == false) {
             if(!Auth::check() && !isset($_GET['token'])) {
                 $_SESSION['redirect'] = Config::get('app.url');
-                header("Location: https://login.vatusa.net/uls/v2/login?fac=ZAU&url=2");
+                header("Location: https://login.vatusa.net/uls/v2/login?fac=ZAU&url=1");
                 exit;
             }
         } else {
             if(!Auth::check() && !isset($_GET['token'])) {
                 $_SESSION['redirect'] = Config::get('app.url');
-                header("Location: https://login.vatusa.net/uls/v2/login?fac=ZAU&url=1");
+                header("Location: https://login.vatusa.net/uls/v2/login?fac=ZAU&url=2");
                 exit;
             }
         }
