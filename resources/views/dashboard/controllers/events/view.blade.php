bccf05c8de63ffed91080380e165fb5d34507351
jrussellzau <50782167+jrussellzau@users.noreply.github.com>
2020-01-07 12:17:58 -0600

    Update view.blade.php

--- resources/views/dashboard/controllers/events/view.blade.php
+++ resources/views/dashboard/controllers/events/view.blade.php
@@ -33,10 +33,10 @@ View Event
             @if(Auth::user()->can('events'))
                 <br>
                 <div class="card">
-                <a href="#collapseCardExample" class="d-block card-header" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="collapseCardExample">
+                <a href="#collapseCardExample" class="d-block card-header" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="assignPositions">
                   <h3>Position Requests</h3>
                 </a>
-				  <div class="collapse show" id="collapseCardExample">
+				  <div class="collapse show" id="assignPositions">
                     <div class="card-body">
                         <p>
                             <i>Assign Positions:</i>
@@ -44,7 +44,7 @@ View Event
                                     <button type="button" class="btn btn-success btn-sm pull-right" data-placement="top">Manual Assign</button>
                                 </span>
                         </p>
-                        <table class="table-sm table-bordered">
+                        <table class="table">
                             <thead>
                             <tr>
 
