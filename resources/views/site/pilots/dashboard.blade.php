c18171849858df3b11986596b9aa8dd6b6165448
jrussellzau <50782167+jrussellzau@users.noreply.github.com>
2020-01-19 16:47:53 -0600

    Update dashboard.blade.php

--- resources/views/site/pilots/dashboard.blade.php
+++ resources/views/site/pilots/dashboard.blade.php
@@ -10,7 +10,7 @@ Welcome
 <br>
 <br>
 
-		<img src="https://picsum.photos/1200/250?blur" class="img-responsive rounded shadow-lg" width="100%" alt="...">
+		<img src="https://picsum.photos/1201/251?blur" class="img-responsive rounded shadow-lg" width="100%" alt="...">
 
     <div class="card o-hidden border-0 shadow-lg my-5">
         <div class="row no-gutters">
@@ -18,7 +18,17 @@ Welcome
 				<img src="https://picsum.photos/800/250?blur" class="card-img img-fluid">
 		  </div>
           <div class="col-lg-5">
-            <div class="p-5">
+            <div class="p-5 text-center">
+				<h3 class="text-primary">
+					Airport Charts & Weather
+				</h3>
+			            {!! Form::open(['action' => 'FrontController@searchAirport']) !!}
+                <div class="form-inline">
+                    {!! Form::text('apt', null, ['placeholder' => 'Search Airport (ICAO)', 'class' => 'form-control']) !!}
+                    &nbsp;
+                    <button class="btn btn-success" type="submit">Search</button>
+                </div>
+            {!! Form::close() !!}
 				</div>
               </div>
             </div>
