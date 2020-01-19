b9a654528f9b58bde38e5b17c666312275eb3650
jrussellzau <50782167+jrussellzau@users.noreply.github.com>
2020-01-19 16:26:28 -0600

    Update dashboard.blade.php

--- resources/views/site/pilots/dashboard.blade.php
+++ resources/views/site/pilots/dashboard.blade.php
@@ -7,12 +7,12 @@ Welcome
 @section('content')
 <div class="shadow-lg p-3 mb-5 bg-primary rounded">
 	<div class="text-center">
-		<img src="https://i.picsum.photos/id/1063/1200/250.jpg" class="rounded" alt="...">
+		<img src="https://picsum.photos/1200/250" class="rounded" alt="...">
 	</div>
 </div>
 <div class="shadow-lg p-3 mb-5 bg-primary rounded">
 	<div class="text-center">
-		<img src="https://i.picsum.photos/id/1063/1200/250.jpg" class="rounded" alt="...">
+		<img src="https://picsum.photos/1200/250" class="rounded" alt="...">
 	</div>
 </div>
 
