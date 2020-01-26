512d6691f92a87e12f959b5b940940186ad6e85c
jrussellzau <50782167+jrussellzau@users.noreply.github.com>
2020-01-26 10:29:52 -0600

    Update activity emails

--- resources/views/emails/inactive/obs.blade.php
+++ resources/views/emails/inactive/obs.blade.php
@@ -4,8 +4,8 @@
 <p>Dear {{ $s->full_name }},</p>
 <br>
 <p>You have not met the activity requirements within the last thirty days. As an observer, you are required to take part in at least 60 minutes of training each three weeks
-. You will have a 30 day grace period to meet this requirement, but after that you may be removed from the roster.</p>
-<p>If you have any concerns or are on an LOA, please contact the DATM at <a href="mailto:datm@chicagoartcc.org">datm@chicagoartcc.org</a>.</p>
+. You will have a 30 day grace period to meet this requirement, but after that you may be removed from the roster. However, we do understand that mentor/instructor availability is limited.</p>
+<p>So, if you have any concerns or are on an LOA, please contact the DATM at <a href="mailto:datm@chicagoartcc.org">datm@chicagoartcc.org</a>.</p>
 <br>
 <p>Sincerely,</p>
 <p>vZAU ARTCC Staff</p>
