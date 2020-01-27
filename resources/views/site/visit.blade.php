5a76570be9f1e92585ebf3ca9873b93995e4caa6
jrussellzau <50782167+jrussellzau@users.noreply.github.com>
2020-01-27 07:51:32 -0600

    Update visit.blade.php

--- resources/views/site/visit.blade.php
+++ resources/views/site/visit.blade.php
@@ -62,7 +62,7 @@ Visitor Application
 
 <!-- Modal -->
 <div class="modal fade" id="visitAgree" tabindex="-1" role="dialog" aria-labelledby="VisitorAgreement" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Required Hours</h5>
